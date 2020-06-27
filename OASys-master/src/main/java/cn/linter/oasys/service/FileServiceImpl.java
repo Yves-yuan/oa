37162b48bde57bd2ef40f030e9a54a4798d99031
yuanp0813@163.com <yuanp0813@163.com>
2020-06-27 16:38:56 +0800

    1

---
+++ OASys-master/src/main/java/cn/linter/oasys/service/FileServiceImpl.java
@@ -0,0 +1,90 @@
+package cn.linter.oasys.service;
+
+import cn.linter.oasys.entity.File;
+import cn.linter.oasys.entity.User;
+import cn.linter.oasys.mapper.FileMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.system.ApplicationHome;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+
+@Service
+public class FileServiceImpl implements FileService {
+    private final FileMapper fileMapper;
+
+    @Autowired
+    public FileServiceImpl(FileMapper fileMapper) {
+        this.fileMapper = fileMapper;
+    }
+
+    @Override
+    public PageInfo<?> getFiles(int userId, int parentId, boolean personal, int pageNumber, int pageSize) {
+        PageHelper.startPage(pageNumber, pageSize);
+        return new PageInfo<>(fileMapper.selectFiles(userId, parentId, personal));
+    }
+
+    @Override
+    public void addFolder(String folderName, int userId, int parentId, boolean personal) {
+        File folder = new File();
+        User user = new User();
+        user.setId(userId);
+        folder.setName(folderName);
+        folder.setPath("/");
+        folder.setSize("-");
+        folder.setType("文件夹");
+        folder.setUser(user);
+        folder.setParentId(parentId);
+        folder.setPersonal(personal);
+        long time = System.currentTimeMillis();
+        folder.setCreateTime(new Timestamp(time));
+        fileMapper.insertFile(folder);
+    }
+
+    @Override
+    public void uploadFile(String fileName, long fileSize, User user, int parentId, boolean personal) {
+        File file = new File();
+        int index = fileName.lastIndexOf('.');
+        file.setName(fileName.substring(0, index));
+        file.setPath("/file/" + fileName);
+        file.setType(fileName.substring(index + 1));
+        file.setSize(formatSize(fileSize));
+        file.setParentId(parentId);
+        file.setPersonal(personal);
+        file.setUser(user);
+        long time = System.currentTimeMillis();
+        file.setCreateTime(new Timestamp(time));
+        fileMapper.insertFile(file);
+    }
+
+    @Override
+    public void renameFile(int id, String newName) {
+        fileMapper.updateFile(id, newName);
+    }
+
+    @Override
+    public void deleteFile(Integer[] ids) {
+        String rootPath = new ApplicationHome(getClass()).getSource().getPath();
+        for (Integer id : ids) {
+            File file = fileMapper.selectFile(id);
+            if (!file.getType().equals("文件夹")) {
+                String path = rootPath + "/static" + file.getPath();
+                java.io.File physicalFile = new java.io.File(path);
+                physicalFile.delete();
+            }
+        }
+        fileMapper.deleteFile(ids);
+    }
+
+    public String formatSize(long size) {
+        if (size < 1024) {
+            return size + "B";
+        } else if (size < 1048576) {
+            return (size >>= 10) + "KB";
+        } else {
+            return (size >>= 20) + "MB";
+        }
+    }
+}

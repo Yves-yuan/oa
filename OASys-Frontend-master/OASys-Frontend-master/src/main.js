251afb9f8dc9b44309d9ffd9cc52178cbc703bc0
yuanp0813@163.com <yuanp0813@163.com>
2020-06-27 17:29:39 +0800

    路径相对写成了绝对切

--- OASys-Frontend-master/OASys-Frontend-master/src/main.js
+++ OASys-Frontend-master/OASys-Frontend-master/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
-import "/styles/index.css";
+import "./styles/index.css";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "./styles/customize.css";
